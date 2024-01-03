068d5d6e545fd9e9a2d4f6970db9384daf1eccfc
Rakhi Mandal <48522324+RakhiMandal@users.noreply.github.com>
2024-01-04 00:05:10 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,4 @@
 here is simple-interest file
+# This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+   # Do not use this in production. Sample purpose only.
